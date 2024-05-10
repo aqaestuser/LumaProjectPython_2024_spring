56949d0eb4806e3d684b632b84721228a61b7709
Cruz06 <104589825+Cruz06@users.noreply.github.com>
2024-05-11 00:47:02 +0300

    Update test_performance_sportswear_new.py

--- tests/test_performance_sportswear_new.py
+++ tests/test_performance_sportswear_new.py
@@ -16,12 +16,14 @@ def test_check_count_of_products(login):
 
 
 @allure.link("https://trello.com/c/9B5bXFEP")
-def test_006_008_001_visibility_of_price_photo_name():
-    # I can't count elements on the page via selen
-    browser.open(PerformanceSportswear.LINK_SPORT)
-    ss(BaseLocators.PRODUCT_NAME).should(have.size(5))
-    ss(BaseLocators.PRODUCT_PRICE).should(have.size(5))
-    ss(BaseLocators.PRODUCT_IMAGE).should(have.size(5))
+def test_006_008_001_visibility_of_price_photo_name_selenium(driver):
+    # I have to find actual nr of elements on the page via python/selenium
+    browser.open(LINK_SPORT)
+    driver.get(LINK_SPORT)
+    nr_of_items_on_page = len(driver.find_elements(By.CSS_SELECTOR, BaseLocators.PRODUCT_ITEM_IN_CATALOG))
+    ss(BaseLocators.PRODUCT_NAME).should(have.size(nr_of_items_on_page))
+    ss(BaseLocators.PRODUCT_PRICE).should(have.size(nr_of_items_on_page))
+    ss(BaseLocators.PRODUCT_IMAGE).should(have.size(nr_of_items_on_page))
 
 
 @allure.link("https://trello.com/c/cmwZ3A6P")
